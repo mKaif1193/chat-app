740ef641acce123547467da63169f989626cbdd0
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-09-28 12:34:10 +0500

    Final deployment

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.frontendUrl],
+    origin: ["https://chat-app-mkaif.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
