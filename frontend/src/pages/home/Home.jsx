e21255a4160e403dbbcbeda4c920920ef71ff365
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-09-28 18:56:44 +0500

    Making message container responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
-    <div className="flex py-4 h-full w-full sm:w-auto sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+    <div className="flex py-4 h-[80%] w-full sm:w-auto sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
       <MessageContainer />
     </div>
