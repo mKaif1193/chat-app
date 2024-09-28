1bc3beed52756e2ef9e61b83b07fc961bd6e31a6
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-09-28 21:16:39 +0500

    Making message container responsive

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -8,12 +8,14 @@ const useListenMessages = () => {
   const { messages, setMessages } = useConversation();
 
   useEffect(() => {
-    socket?.on("newMessage", (newMessage) => {
-      newMessage.shouldShake = true;
-      const sound = new Audio(notificationSound);
-      sound.play();
-      setMessages([...messages, newMessage]);
-    });
+    if (socket) {
+      socket?.on("newMessage", (newMessage) => {
+        newMessage.shouldShake = true;
+        const sound = new Audio(notificationSound);
+        sound.play();
+        setMessages([...messages, newMessage]);
+      });
+    }
 
     return () => socket?.off("newMessage");
   }, [socket, setMessages, messages]);
