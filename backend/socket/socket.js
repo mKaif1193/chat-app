7b1c5a37a6d390e6a152318a5e334f5d7e0446fd
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-09-28 20:23:40 +0500

    Making message container responsive

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,17 +1,13 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-import cors from "cors";
 
 const app = express();
-
-app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    // origin: ["https://chat-app-mkaif.vercel.app"],
-    origin: "*",
+    origin: ["https://chat-app-mkaif.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
