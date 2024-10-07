e03d844230bcb0e7a32703cf949cf76b2be1f047
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-10-07 15:38:02 +0500

    Updating Socket.IO in Backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
@@ -11,6 +12,7 @@ import userRoutes from "./routes/user.routes.js";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 connectToMongoDB();
